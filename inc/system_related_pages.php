fbf48d35497f8f46c5366f132f222887d22c7d2a
mohamed-yassin <mhmd.yassin@yahoo.com>
2020-09-24 14:31:41 +0200

    missing strings

--- inc/system_related_pages.php
+++ inc/system_related_pages.php
@@ -8,7 +8,7 @@ function robo_add_admin_menu() {
 function robo_orders_admin_page_render(){
 
 	// Page Header
-	echo "<h2> Subscriptions Orders</h2>";
+	echo "<h2>".__('Subscriptions Orders','robo')."</h2>";
 
 	// prepare data
 	$customer_subscriptions = get_posts( 
@@ -19,10 +19,10 @@ function robo_orders_admin_page_render(){
 		)
 	);
 	$conditions =  array(
-		'wc-active' 	=> 'Active',
-		'wc-on-hold' 	=> 'On Hold',
-		'wc-cancelled' 	=> 'Cancelled',
-		'wc-pending' 	=> 'Pending',
+		'wc-active' 	=> __('Active','robo'),
+		'wc-on-hold' 	=> __('On Hold','robo'),
+		'wc-cancelled' 	=> __('Cancelled','robo'),
+		'wc-pending' 	=> __('Pending','robo'),
 	);
 	foreach ($customer_subscriptions as $key => $sub) {
 		$table_body[$sub->ID]['ID'] 			=  $sub->ID;
@@ -30,24 +30,24 @@ function robo_orders_admin_page_render(){
 		//$table_body[$sub->ID]['post_author'] 	=  "<a href='".get_edit_user_link($sub->post_author)."'>".not_null_auther_name($sub->post_author)."</a>";
 		$table_body[$sub->ID]['post_status'] 	=  $conditions[$sub->post_status];
 		$table_body[$sub->ID]['slug'] 			=  get_field('slug',$sub->ID);
-		$table_body[$sub->ID]['gate_way'] 		=  get_field('gate_way',$sub->ID) == '1'  ?  __("Chat API") : __("Not Defined Yet");
-		$table_body[$sub->ID]['details'] 		=  "<a href='".get_edit_post_link($sub->ID)."'>".__('details')."</a>";
+		$table_body[$sub->ID]['gate_way'] 		=  get_field('gate_way',$sub->ID) == '1'  ?  __("Chat API",'robo') : __("Not Defined Yet",'robo');
+		$table_body[$sub->ID]['details'] 		=  "<a href='".get_edit_post_link($sub->ID)."'>".__('details','robo')."</a>";
 	}
 	$header = array(
-		'id'			=> __('ID'),
-		'post_title' 	=> __('Name'), 
+		'id'			=> __('ID','robo'),
+		'post_title' 	=> __('Name','robo'), 
 		//'post_author' 	=> 'Client',
-		'config' 	    => __('Status'),
-		'slug' 	    	=> __('Slug'),
-		'gate_way'		=> __('Gate Way'),
-		'details'		=> __('Subscription Details'),
+		'config' 	    => __('Status','robo'),
+		'slug' 	    	=> __('Slug','robo'),
+		'gate_way'		=> __('Gate Way','robo'),
+		'details'		=> __('Subscription Details','robo'),
 	);
 	render_table($header,$table_body);
 }
 function client_dashboard(  ) { 
 	$user_blogs 	= subs_option_field_array();
 	if(get_current_blog_id() != 1  &&  is_array($user_blogs)){
-		add_menu_page( "RoboChat Settings", "RoboChat Settings", 'read', "robochat_settings", 'roboChat_options_page' , '' , 3);
+		add_menu_page( "RoboChat Settings RRR", "RoboChat Settings", 'read', "robochat_settings", 'roboChat_options_page' , '' , 3);
 		$options = get_option( 'roboChat_settings' );
 		foreach ($user_blogs as $key => $blog) {
 			$blog_id =  is_array($blog) ?  $blog['slug'] :  $blog;  
@@ -57,18 +57,15 @@ function client_dashboard(  ) {
 	}
 }
 function robochat_settings(){ ?>
-	<h2><?=  __('الاعدادات العامه'); ?></h2>
+	<h2><?=  __('General Settings','robo'); ?></h2>
 	<form action='options.php' method='post'>
-	<h3><?=  __('اعاده تسميه الاشتراكات') ; ?></h3>
-
-			<?php
-			settings_fields( 'roboChat_settings' );
-			do_settings_sections( 'roboChat_settings' );
-			submit_button();
-			?>
-
-		</form>
-
+	<h3><?=  __('Rename Subscriptions','robo') ; ?></h3>
+		<?php
+		settings_fields( 'roboChat_settings' );
+		do_settings_sections( 'roboChat_settings' );
+		submit_button();
+		?>
+	</form>
 <?php 
 }
 function whatsappapi_options_page() { 
@@ -82,7 +79,7 @@ function whatsappapi_options_page() {
 		$token 				 = $sub_connection_data['token'];
 
 		if($api == '' || $token == ''){
-			echo "<h2>" .  __('"من فضلك تواصل مع قسم المبيعات او الدعم الفني للتحقق من تفعيل حسابك"') . "</h2>" ;
+			echo "<h2>" .  __('PLease contact Sales Department to check your account activation','robo') . "</h2>" ;
 		}else {
 			$chatapi_sub_status =  chatapi_sub_status($api,$token);
 			if(isset($chatapi_sub_status['accountStatus']) && $chatapi_sub_status['accountStatus'] == 'authenticated'){
@@ -112,7 +109,7 @@ function whatsappapi_options_page() {
 					}elseif ($process  == 'show_msgs') {
 						
 					}else {
-						_e('عذرا رابط  غير صحيح او لا تملك الصلاحيات') ; 
+						_e('Sorry, the link is incorrect or you do not have the permissions','robo') ; 
 					}
 				}
 			}else {
@@ -120,16 +117,13 @@ function whatsappapi_options_page() {
 					pre($chatapi_sub_status);
 				}
 				if(isset($chatapi_sub_status['accountStatus']) &&  $chatapi_sub_status['accountStatus'] == 'loading') {
-					_e("هناك مشكله :: </br>
-					1-  تاكد ان هاتفك متصل بالانترنت </br>
-					2- وفتح برنامج الواتساب علي الموبايل </br>
-					3 - انهاء توصيل الواتساب الخاص بك مع اي خدمات اخري  </br></br>
+					_e("there is a problem :: if you reload the page and still exist , </br>",'robo');
+					_e("1- Make sure that your phone is connected to the Internet</br>",'robo');
+					_e("2- And opening the WhatsApp program on the mobile</br>",'robo');
+					_e("3 - Terminate your WhatsApp connection with any other services</br>",'robo');
 
-					او اعد عمل الاتصال
-				") ; 
 				}elseif (! isset($chatapi_sub_status['accountStatus'])) {
-					_e('هناك مشكله في معلومات الاتصال ,  , من فضلك راسل خدمه العملاء') ; 
-
+					_e('There is a problem with the contact information, please contact customer service') ; 
 				}else{
 					$data['qr'] = $chatapi_sub_status['qrCode'] ;
 					view('qr_code', $data);
@@ -137,7 +131,7 @@ function whatsappapi_options_page() {
 			}
 		}
 	}else {
-		_e('لا يمكن الدخول الي الحساب من فضلك راسل الاداره لتفعيل الحساب') ; 
+		_e('The account cannot be accessed. Please contact the customer service to activate the account') ; 
 	}
 }
 
